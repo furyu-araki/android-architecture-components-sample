35b4e75ac95f6e06e7e15ba246b945ff8fc32c72
furyu-araki <araki.taku@furyu.jp>
2017-06-15 12:53:11 +0900

    put RecyclerView.

--- app/src/main/java/com/example/arakitaku/aactwitterclient/MainActivity.java
+++ app/src/main/java/com/example/arakitaku/aactwitterclient/MainActivity.java
@@ -1,17 +1,15 @@
 package com.example.arakitaku.aactwitterclient;
 
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
 
-import com.twitter.sdk.android.core.TwitterCore;
-
 public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,14 +18,7 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                
-
-            }
-        });
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
     }
 
     @Override
